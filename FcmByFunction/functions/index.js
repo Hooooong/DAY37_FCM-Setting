c5245204050dde003533ba1bed89f58a15c7f91e
Hooooong <h921013@gmail.com>
2017-11-05 03:46:41 +0900

    코드 수정

--- FcmByFunction/functions/index.js
+++ FcmByFunction/functions/index.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 
 admin.initializeApp(fun.config().firebase);
 
+// 1. Firebase Function 을 통해 Firebase Realtime Database 에 값을 넣기
 exports.addMessage = fun.https.onRequest((req, res)=>{
     // http 요청에서
     // ? 다음에 있는 변수중에 text 변수를 가져온다.
@@ -22,32 +23,40 @@ exports.addMessage = fun.https.onRequest((req, res)=>{
 
 exports.sendNotification = fun.https.onRequest((req, res)=>{
 
-	// JSON Data Post 값을 수신
+	// 1. JSON Data Post 값을 수신
+	// req.body 에는 {"to":"상대방의 Token값", "msg":"블라블라블라"} 로 이루어져 있다.
+	// req.body 내용을 추가적으로 보내 Title, body, click_action, sound 를 변경할 수 있다.
 	var dataObj = req.body;
 	// 전송할 메시지 객체를 완성
 	var msg = {
 		notification : {
 			title : "노티바에 나타나는 타이틀",
 			body : dataObj.msg,
-			// Action Name
+
 			// intent-filter 의 action- name 의 값을 넣는다.
 			// Default 값을 넣어야 인식 한다. 
+			// ==== Android Manifest.xml ====
 			// <action android:name="NOTI_LAUNCHER" />
 			// <category android:name="android.intent.category.DEFAULT" />
-			//click_action : "NOTI_LAUNCHER",
+			click_action : "NOTI_LAUNCHER",
 			// res/raw/파일명 을 작성하면 된다.
-			//sound: "kick2.wav"
+			sound: "kick2.wav"
 		}
 	};
 	
+	// Token 값을 배열로 해야 한다.
 	var tokens =[];
 	tokens.push(dataObj.to);
+
 	admin.messaging()
-		.sendToDevice(tokens, msg)
-		.then(function(response){
-			res.status(200).send(response);
-		}).catch(function(error){
-			res.status(500).send(error);
-		});
+	.sendToDevice(tokens, msg)
+	// 요청이 정상적인지에 대한 콜백
+	.then(function(response){
+		res.status(200).send(response);
+	})
+	// 요청에 대한 실패
+	.catch(function(error){
+		res.status(500).send(error);
+	});
 	
 });
--- FcmByNode/server.js
+++ FcmByNode/server.js
@@ -7,18 +7,29 @@ const fcmServerUrl = "https://fcm.googleapis.com/fcm/send";
 const serverKey = "AAAAETU6DsQ:APA91bFccddKUp065VAK8zpEI-9AtuxyUflFATQ7ysFVGSj4tpPvtSyMC-isDVv2BhzXgIdpq3PKFaQ_pQKbb3bJbpyrxGo1wtwC6xofHRFSzjO4wpQvIYWjYYHgDp3zkEer5OQI2pXm"
 
 var msg = {
+    // 누구에게 보낼것인가를 지정
+    // Token 값을 설정한다.
     to : "",
+
+    // App 이 꺼져있을 때의 Notification 속성
+    // title : Notification Title(제목)
+    // body : Notification body(내용)
+    // click_action : Notification 을 눌렀을 때 이동할 Activity 의 action_name을 적는다.
+    // sound : Notification sound(알람 소리)  
     notification : {
         title : "Message Test!",
         body : "",
-        // Action Name
+
         // intent-filter 의 action- name 의 값을 넣는다.
         // Default 값을 넣어야 인식 한다. 
+        // ==== Android Manifest.xml ====
         // <action android:name="NOTI_LAUNCHER" />
         // <category android:name="android.intent.category.DEFAULT" />
         click_action : "NOTI_LAUNCHER",
         sound: "kick2.wav"
     },
+    // App 이 켜져 있을 때, Notification 에 데이터(Title, Body 등)를 보내기위해 쓰는 속성
+    // data 에 담아서 보내면 된다.
     data :{
         type:"one",
         content : ""
@@ -36,9 +47,12 @@ var server = http.createServer(function(req,res){
     
         // 메세지 수신 완료
         req.on('end', function(){
-            // JSON String 을 객체로 변환
+            // 1. JSON String 을 객체로 변환
             var postObj = JSON.parse(post_data);
-            // 메세지 데이터 변환
+            
+            // 2. 보낼 Notification 변환
+            // 2-1. 받은 데이터를 msg.data 에 담아서 보낸다. (App 이 켜져 있을 때, Notification의 Title, Body 를 변환해야 되기 때문에)
+            // 또는 msg.notification.body 에 담아서 보낸다. (App 이 꺼져 있을 때, Notification의 Title, Body 를 변환해야 되기 때문에)
             msg.to = postObj.to;
             msg.notification.body = postObj.msg;
             msg.data.content = postObj.msg;
